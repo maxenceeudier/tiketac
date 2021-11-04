01050f70159b5e2980090b9e683515f7ca609642
Maxence Eudier <eudier.maxence@gmail.com>
2021-11-04 10:00:49 +0100

    connection db

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var options = {
  };
 
 // --------------------- BDD -----------------------------------------------------
-mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
+mongoose.connect('mongodb+srv://maxence:maxenceeudier@cluster0.hlgpy.mongodb.net/tiketac?retryWrites=true&w=majority',
    options,
    function(err) {
     if (err) {
